0430cb45af096dcb733f2a19a1aedac4401232c0
jfsantos011 <72039878+jfsantos011@users.noreply.github.com>
2021-03-17 21:47:37 -0300

    Correção do Código e exclusão da pasta SRC
    Correção do código e Exclusão da pasta SRC

--- App.js
+++ App.js
@@ -1,81 +1,85 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
-import ImagemTT from './src/components/ImagemTT';
-import AreaFrase from './src/components/AreaFrase';
-import Botao from './src/components/Botao';
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default class App extends Component {
-
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
     this.state = {
-      frase: ''
-    };
-    this.geraNovaFrase = this.geraNovaFrase.bind(this);
+      imgIndex: 0,
+      phraseIndex: 0,
+      images: [
+        'https://cdn.pensador.com/img/imagens/pa/ul/paulo_vanzolini_levanta_sacode_a_poeira_da_volta_por_cima_1.jpg',
+        'https://cdn.pensador.com/img/imagens/ne/mt/nem_todos_os_dias_sao_bons_mas_ha_algo_bom.jpg',
+        'https://cdn.pensador.com/img/imagens/ma/rt/martin_luther_king_suba_primeiro_degrau_com_fe.jpg',
+        'https://cdn.pensador.com/img/imagens/to/da/toda_manha_voce_tem_duas_escolhas_continuar_dormindo.jpg',
+        'https://cdn.pensador.com/img/imagens/fr/as/frases_positivas_para_o_dia_1.jpg',
+        'https://cdn.pensador.com/img/imagens/fr/as/frases_positivas_para_o_dia_2_0.jpg'
+      ],
+      phrases: [
+        'Enfrente os problemas e leve a melhor!',
+        'Dê mais atenção ao que você tem de bom na sua vida!',
+        'Para chegar ao topo, o que importa é começar!',
+        'De nada adianta ter sonhos, se você não se empenhar em correr atrás',
+        'Preste atenção nas oportunidades que estão à sua frente!',
+        'Positividade para começar o dia é colocar a sua fé em prática'
+      ]
+    }
   }
 
-  geraNovaFrase() {
-
-    let numRandom = Math.random();
-    numRandom = Math.floor(numRandom * 5);
-
-    var frases = Array();
-    frases[0] = '"Enfrente os problemas e leve a melhor!"';
-    frases[1] = '"Dê mais atenção ao que você tem de bom na sua vida"';
-    frases[2] = '"Para chegar ao topo, o que importa é começar!"';
-    frases[3] = '"De nada adianta ter sonhos, se você não se empenhar em correr atrás"';
-    frases[4] = '"Preste atenção nas oportunidades que estão à sua frente!"';
-
-    let fraseEscolhida = frases[numRandom];
-
-    this.setState({ frase: fraseEscolhida });
-
-
-  }
-
-  render() {
+  render () {
     return (
-      <View style={{
-        backgroundColor: 'white',
-        marginItens: 100,
-        alignItems: 'center'
-      }}>
-
-
-        <AreaFrase
-          frase={this.state.frase}
-        />
-        <ImagemTT img={this.state.img}
-          largura={400}
-          altura={200}
-          alinha={'center'}
+      <View style={styles.container}>
+        <Image
+          source={{ uri: this.state.images[this.state.imgIndex] }}
+          style={styles.image}
         />
 
-        <Botao
-          style={styles.bottun}
-          geraNovaFrase={this.geraNovaFrase}
-          onPress={this.enviar}
-        />
+        <Text style={styles.phrase}>{this.state.phrases[this.state.phraseIndex]}</Text>
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={() => {
+            this.setState({
+              ...this.state,
+              imgIndex: Math.floor(Math.random() * (6 + 1)),
+              phraseIndex: Math.floor(Math.random() * (6 + 1))
+            })
+          }}
+        >
+          <Text style={styles.btnText}>Abrir frase do dia</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-
-  },
-  img: {
+    backgroundColor: '#fff',
     alignItems: 'center',
-    margin: 50,
     justifyContent: 'center',
-    alignItems: 'center'
-
+    textAlign: 'center'
   },
-
-
-})
+  image: {
+    width: 300,
+    height: 200,
+    borderRadius: 4
+  },
+  phrase: {
+    marginVertical: 16
+  },
+  button: {
+    width: 300,
+    height: 30,
+    borderColor: 'red',
+    borderWidth: 1,
+    borderRadius: 4,
+    marginVertical: 8,
+    paddingVertical: 5
+  },
+  btnText: {
+    textAlign: 'center'
+  }
+});
